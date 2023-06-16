82fc501602a44e94aee8cec8e8d480d32068058e
Karan7zler <23karan2gmail.com>
2023-06-16 15:23:10 +0530

    Aron Codes

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -2,9 +2,18 @@ package org.test;
 
 public class Sample {
 	
+	private void method1() {
+		System.out.println("ARON");
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
+		Sample s =new Sample();
 		
+		s.method1();
 		
 		
 		
